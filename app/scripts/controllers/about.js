e613b9138e8ce2b778d6f7081ba60a1ed2d197b1
Paul Jobson <pjobson@gmail.com>
2014-10-31 10:35:04 -0400

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name actorGraphor.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the actorGraphor
+ */
+angular.module('actorGraphor')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name actorGraphor.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the actorGraphor
+ */
+angular.module('actorGraphor').controller('MainCtrl', function ($scope) {
+	$scope.awesomeThings = [
+		'HTML5 Boilerplate',
+		'AngularJS',
+		'Karma'
+	];
+
+	
+});
